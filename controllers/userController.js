d9d6a90140a13b29ed657cfbbe64a6d25c132b9b
Moosa Raheel <moosarahil13@gmail.com>
2023-09-08 22:29:52 +0500

    roughly login backend complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,25 @@ class userController {
   static home = (req, res) => {
     res.render("index");
   };
+  static verifyUser = async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = await UserModel.findOne({ email: username });
+      if (user) {
+        const hashPassword = await bcrypt.compare(password, user.password);
+        if (hashPassword) {
+          console.log("login successfully");
+        } else {
+          console.log("Password is incorrect");
+        }
+      } else {
+        console.log("Account not found");
+      }
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   static createUser = async (req, res) => {
     try {
       const { fname, sname, email, password, dob, gender } = req.body;
--- routes/web.js
+++ routes/web.js
@@ -2,5 +2,6 @@ import { Router } from "express";
 import { userController } from "../controllers/userController.js";
 const router = Router();
 router.get("/", userController.home);
+router.post("/", userController.verifyUser);
 router.post("/signup", userController.createUser);
 export default router;
